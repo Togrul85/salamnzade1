127f6dbf257a40a0ba08d4bd08823832ddef8241
unknown <toghrul@code.edu.az>
2022-08-03 18:07:36 +0400

    .

---
+++ task20.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 2, 5, 9, 10 };
+            int max = 0;
+            for (int i = 0; i < arr.Length; i++)
+                
+            
+            {   
+                {
+                    if (arr[i]>max) ;
+                   
+                }
+                Console.WriteLine(max);
+            }
+        }
+			
+
+			}
+    }
+
